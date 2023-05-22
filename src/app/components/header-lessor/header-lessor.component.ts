00769312176e0f136014a1174af046119308424f
Leydi Yaquelin Mamani Aro <u20201b745@upc.edu.pe>
2023-05-21 22:48:41 -0500

    add header-lessor component

---
+++ src/app/components/header-lessor/header-lessor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-lessor',
+  templateUrl: './header-lessor.component.html',
+  styleUrls: ['./header-lessor.component.css']
+})
+export class HeaderLessorComponent {
+
+}
