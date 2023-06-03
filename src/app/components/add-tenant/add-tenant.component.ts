fc9687c16c2e161a770f2483c5430d1e12b4eb3a
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-06-02 20:30:30 -0500

    Create add-tenant.component.ts

---
+++ src/app/components/add-tenant/add-tenant.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tenant',
+  templateUrl: './add-tenant.component.html',
+  styleUrls: ['./add-tenant.component.css']
+})
+export class AddTenantComponent {
+
+}
