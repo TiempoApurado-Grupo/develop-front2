602106fbb1bfc82d1038be7f8dabb486e495b0df
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-06-10 01:51:14 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
   {path:'navigation/add/tenant', component:AddTenantComponent},
   {path: 'lessors', component: PostsComponent},
   {path: 'lessors/:lessorId/posts/:postId', component: ViewPostsComponent},
-
+  
 ];
 
 @NgModule({
